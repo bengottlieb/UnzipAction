42bb93f9cb623e3dd50edaa8d6942d24ae5b05c8
Ben Gottlieb <ben@standalone.com>
2024-02-15 16:19:51 -0600

    Initial Commit

---
+++ UnzipAction/UnzipActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnzipActionApp.swift
+//  UnzipAction
+//
+//  Created by Ben Gottlieb on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct UnzipActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
